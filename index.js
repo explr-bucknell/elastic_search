e744c9ffca81714be5df1922267a555d01ac9b6b
jw057 <jw057@bucknell.edu>
2018-02-25 20:05:42 -0500

    elastic search init

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const path = require('path')
 var cool = require('cool-ascii-faces')
 const PORT = process.env.PORT || 5000
 
+var bonsai_url = process.env.BONSAI_URL;
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: bonsai_url,
+  log: 'trace'
+});
+
+// Test the connection:
+// Send a HEAD request to "/" and allow
+// up to 30 seconds for it to complete.
+client.ping({
+	requestTimeout: 30000,
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
