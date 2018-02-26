334be6a03439ea8fa8a880601351534c948844da
jw057 <jw057@bucknell.edu>
2018-02-26 14:22:51 -0500

    basic functionalities

--- index.js
+++ index.js
@@ -1,32 +1,119 @@
 const express = require('express')
 const path = require('path')
-var cool = require('cool-ascii-faces')
+const elasticsearch = require('elasticsearch')
+const firebase = require('firebase')
+
 const PORT = process.env.PORT || 5000
 
-var bonsai_url = process.env.BONSAI_URL;
-var elasticsearch = require('elasticsearch');
+var bonsai_url = process.env.BONSAI_URL
+
 var client = new elasticsearch.Client({
   host: bonsai_url,
   log: 'trace'
-});
+})
 
-// Test the connection:
-// Send a HEAD request to "/" and allow
-// up to 30 seconds for it to complete.
-client.ping({
-	requestTimeout: 30000,
-}, function (error) {
-  if (error) {
-    console.error('elasticsearch cluster is down!');
-  } else {
-    console.log('All is well');
-  }
-});
+var config = {
+  apiKey: 'AIzaSyBztce7Z8iOrB5EgV4IE8gjlFGAy6MXSkQ',
+  authDomain: 'senior-design-explr.firebaseapp.com',
+  databaseURL: 'https://senior-design-explr.firebaseio.com',
+  projectId: 'senior-design-explr',
+  storageBucket: 'senior-design-explr.appspot.com',
+  messagingSenderId: '866651490806'
+}
+firebase.initializeApp(config)
+
+var getPlacesData = (callback, res) => {
+  const ref = firebase.database().ref('pois')
+  ref.once('value', function(snapshot) {
+    if (snapshot.numChildren()) {
+      var data = []
+      snapshot.forEach(function(poi) {
+        data.push({
+          index: {
+            _index: 'explr',
+            _type: 'places',
+            _id: poi.val().id
+          }
+        })
+        data.push({
+          name: poi.val().name,
+          id: poi.val().id
+        })
+      })
+      callback(data, res)
+    }
+  })
+}
+
+var initPlaces = (data, res) => {
+  client.bulk({
+    body: data
+  }, function (error, response) {
+    // ...
+    console.log("initPlaces", response)
+    res.send(response)
+  })
+}
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  .get('/', (req, res) => res.send('explr'))
+
+  // ping cluster for connection testing
+  .get('/ping', (req, res) => {
+    client.ping({
+      requestTimeout: 30000,
+    }, function (error) {
+      if (error) {
+        console.error('elasticsearch cluster is down!')
+        res.send('elasticsearch cluster is down!')
+      } else {
+        console.log('All is well')
+        res.send('All is well')
+      }
+    })
+  })
+
+  // initialize the places index by reading firebase data at once
+  .get('/init_places', (req, res) => {
+    client.deleteByQuery({
+      index: 'explr',
+      type: 'places',
+      body: {
+        query: {
+          match_all: {}
+        }
+      }
+    }, function (error, response) {
+      // ...
+    })
+    getPlacesData(initPlaces, res)
+  })
+
+  // update the places index when called by cloud functions which listens to firebase changes
+  .get('/update_places', (req, res) => {
+    console.log("request", req.query)
+    var data = JSON.parse(req.query.data)
+
+    client.bulk({
+      body: data
+    }, function (error, response) {
+      // ...
+      console.log("updatePlaces", response)
+      res.send(response)
+    })
+  })
+
+  // search for places and pass in a field q which is the query
+  .get('/search_places', (req, res) => {
+    var query = req.query.q
+    client.search({
+      index: 'explr',
+      type: 'places',
+      q: 'name:' + query
+    }, function (error, response) {
+      // ...
+      res.send(response)
+    })
+  })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
